c553892738e1f2e81e5891665078db0cc19bc1fb
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-06-27 10:21:34 +0100

    added font size file

--- lib/cores/constants/font_size.dart
+++ lib/cores/constants/font_size.dart
@@ -1,9 +1,17 @@
-import 'package:flutter/widgets.dart';
-
+/// very tiny text font size
 const double kfsVeryTiny = 12.0;
-const double kfsTiny = 14.0;
-const double kfsMeduim = 16.0;
-const double kfsLarge = 18.0;
-const double kfsExtraLarge = 20.0;
-const double kfsSuperLarge = 20.0;
 
+/// tiny text font size
+const double kfsTiny = 14.0;
+
+/// medium text font size
+const double kfsMeduim = 16.0;
+
+/// large text font size
+const double kfsLarge = 18.0;
+
+/// extra large text font size
+const double kfsExtraLarge = 20.0;
+
+/// super large(H1) text font size
+const double kfsSuperLarge = 20.0;
