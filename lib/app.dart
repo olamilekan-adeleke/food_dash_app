2fea11075c1a9ea697a4f6e7f321e94f59f6fb0d
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-06-27 10:23:10 +0100

    made navigation service variable static

--- lib/app.dart
+++ lib/app.dart
@@ -8,15 +8,19 @@ import 'cores/utils/route_name.dart';
 import 'cores/utils/router.dart';
 
 class MyApp extends StatelessWidget {
-  final NavigationService navigationService =
+  
+  static final NavigationService navigationService =
       GetIt.instance<NavigationService>();
+
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: blocList(context),
       child: MaterialApp(
         title: 'Food Dash',
-        theme: ThemeData(primarySwatch: Colors.blue),
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
         navigatorKey: navigationService.navigatorKey,
         onGenerateRoute: generateRoute,
         initialRoute: RouteName.inital,
