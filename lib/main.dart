ae5cec5ad6d58985c87a7af2b7cfeca681faf492
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-06-29 13:01:45 +0100

    addded snack bar config

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:food_dash_app/cores/utils/snack_bar_config.dart';
 
 import 'app.dart';
 import 'cores/utils/locator.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  setUp();
+  setUpLocator();
+  Config.setUpSnackBarConfig();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
