0f9ebbe8eb72605f21e7c5a67ce667c1ba554d3a
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-06-29 10:37:33 +0100

    impelmented fetching and removing of item form favourites and cart

--- lib/features/food/repo/food_repo.dart
+++ lib/features/food/repo/food_repo.dart
@@ -1,10 +1,16 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:food_dash_app/features/auth/repo/auth_repo.dart';
 import 'package:food_dash_app/features/food/model/food_product_model.dart';
 import 'package:food_dash_app/features/food/model/merchant_model.dart';
+import 'package:get_it/get_it.dart';
 
 class MerchantRepo {
+  static AuthenticationRepo authenticationRepo =
+      GetIt.instance<AuthenticationRepo>();
   static final CollectionReference<Map<String, dynamic>> foodCollectionRef =
       FirebaseFirestore.instance.collection('food');
+  static final CollectionReference<Map<String, dynamic>> userCollectionRef =
+      FirebaseFirestore.instance.collection('users');
   final int limit = 10;
 
   Future<List<MerchantModel>> getMerchant({MerchantModel? lastMerchant}) async {
@@ -59,5 +65,55 @@ class MerchantRepo {
     );
 
     return merchants;
+  }
+
+  Future<void> addToFavourite(FoodProductModel foodProductModel) async {
+    final String? userId = authenticationRepo.getUserUid();
+
+    await userCollectionRef
+        .doc(userId)
+        .collection('favourites')
+        .doc(foodProductModel.id)
+        .set(foodProductModel.toMap());
+  }
+
+  Future<void> removeFromFavourite(String foodProductId) async {
+    final String? userId = authenticationRepo.getUserUid();
+
+    await userCollectionRef
+        .doc(userId)
+        .collection('favourites')
+        .doc(foodProductId)
+        .delete();
+  }
+
+  Future<void> addToCart(FoodProductModel foodProductModel) async {
+    final String? userId = authenticationRepo.getUserUid();
+
+    await userCollectionRef
+        .doc(userId)
+        .collection('cart')
+        .doc()
+        .set(foodProductModel.toMap());
+  }
+
+  Future<void> updateCartItem(FoodProductModel foodProductModel) async {
+    final String? userId = authenticationRepo.getUserUid();
+
+    await userCollectionRef
+        .doc(userId)
+        .collection('cart')
+        .doc(foodProductModel.id)
+        .update(foodProductModel.toMap());
+  }
+
+  Future<void> removeFromCart(String foodProductId) async {
+    final String? userId = authenticationRepo.getUserUid();
+
+    await userCollectionRef
+        .doc(userId)
+        .collection('cart')
+        .doc(foodProductId)
+        .delete();
   }
 }
