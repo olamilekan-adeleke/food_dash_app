02dea45f518285da80ea5c368e6e938944faacd2
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-27 09:16:11 +0100

    update

--- functions/index.js
+++ functions/index.js
@@ -424,28 +424,6 @@ async function addDataToPopular(docId, data) {
  *  @param {string} path the path.
  * @param {string} data The data.
  */
-async function incrementTotalOrderCount() {
-  const d = new Date();
-
-  const day = d.getDate(); // Day		[dd]	(1 - 31)
-  const month = d.getMonth() + 1; // Month	[mm]	(1 - 12)
-  const year = d.getFullYear(); // Year		[yyyy]
-
-  return await admin
-    .firestore()
-    .collection("constants")
-    .doc("metrics")
-    .collection(`${year.toString()}`)
-    .doc(`${month.toString()}`)
-    .collection("days")
-    .doc(`${day.toString()}`)
-    .set(
-      {
-        total_order: admin.firestore.FieldValue.increment(1),
-      },
-      { merge: true }
-    );
-}
 
 /**
  * save data to firestore
