09cad4e184a1d60a2529233166125ff066907893
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-11-27 10:03:23 +0100

    update and fixes

--- functions/index.js
+++ functions/index.js
@@ -4,79 +4,11 @@ const admin = require("./firebase_");
 const sendNotificationToAllUserHttpFunction = require("./src/https/send_notification_to_all_user_http_funtion");
 const onOrderStatusChangedFunction = require("./src/reactive/on_order_status_changed_function");
 const OnNewOrderCreatedUpdateShopStat = require("./src/reactive/on_new_order_created_update_shop_stat_function");
+const onNewOrderCreated = require("./src/reactive/on_new_order_created");
 
 exports.OnNewOrderCreated = functions.firestore
   .document("/orders/{ordersID}")
-  .onCreate(async (snapshot, context) => {
-    console.log(context);
-    const data = snapshot.data();
-    const userId = data.user_details.uid;
-    const orderId = data.id;
-    const docId = uuidv4();
-    const items = data.items;
-    const itemFee = data.items_fee;
-    const type = data.type;
-
-    const sendToCustomer = {
-      data_to_send: "msg_from_the_cloud",
-      click_action: "FLUTTER_NOTIFICATION_CLICK",
-      order_id: `${orderId}`,
-    };
-
-    const dataToSave = {
-      body: "Your Order Was Succseffuly Made!",
-      orderId: `${orderId}`,
-      timestamp: admin.firestore.Timestamp.now(),
-      userId: `${userId}`,
-      id: `${docId}`,
-      items: items,
-    };
-
-    // / send out notifications
-    await sendNotificationToUser(
-      userId,
-      "Your Order Was Succseffuly Made!",
-      sendToCustomer
-    );
-
-    await sendNotificationToUser(
-      "riders",
-      "A New Order Was Just Made!. Login to accept order",
-      sendToCustomer
-    );
-
-    // / update user noticfication
-    await saveDataToUserNotification(userId, docId, dataToSave)
-      .then(() => {
-        console.info("succesfully: saved notification data");
-      })
-      .catch((error) => {
-        console.info("error in execution: notification not saved");
-        console.log(error);
-        return { msg: "error in execution: notification not saved" };
-      });
-
-    // update like count
-    if (type === "food") {
-      // / add order to favourite
-      items.forEach(async (element) => {
-        const id = element.id;
-        const dataTo = {
-          likes_count: admin.firestore.FieldValue.increment(1),
-        };
-
-        await addDataToPopular(id, dataTo);
-      });
-    }
-
-    // update total count order count for that day
-    await incrementTotalOrderCount();
-
-    // update item fee
-    await incrementTotalOrderAmountCount(itemFee);
-
-    return Promise.resolve();
-  });
+  .onCreate(onNewOrderCreated);
 
 exports.OnNewOrderCreatedUpdateShopStat = functions.firestore
   .document("/orders/{ordersID}")
--- functions/src/reactive/on_new_order_created.js
+++ functions/src/reactive/on_new_order_created.js
@@ -1,3 +1,80 @@
+const { v4: uuidv4 } = require("uuid");
+const admin = require("./firebase_");
+const sendNotificationToUser = require("../controllers/send_notification_to_user");
+const saveDataToUserNotification = require("../controllers/save_data_to_user_notification_collection");
+const addDataToPopular = require("../controllers/add_data_to_popular");
+const incrementTotalOrderCount = require("../controllers/increment_total_order_count");
+const incrementTotalOrderAmountCount = require("../controllers/increment_completed_order_count");
+
 const onNewOrderCreated = async (snapshot, context) => {
-    
- };
+  console.log(context);
+  const data = snapshot.data();
+  const userId = data.user_details.uid;
+  const orderId = data.id;
+  const docId = uuidv4();
+  const items = data.items;
+  const itemFee = data.items_fee;
+  const type = data.type;
+
+  const sendToCustomer = {
+    data_to_send: "msg_from_the_cloud",
+    click_action: "FLUTTER_NOTIFICATION_CLICK",
+    order_id: `${orderId}`,
+  };
+
+  const dataToSave = {
+    body: "Your Order Was Succseffuly Made!",
+    orderId: `${orderId}`,
+    timestamp: admin.firestore.Timestamp.now(),
+    userId: `${userId}`,
+    id: `${docId}`,
+    items: items,
+  };
+
+  // / send out notifications
+  await sendNotificationToUser(
+    userId,
+    "Your Order Was Succseffuly Made!",
+    sendToCustomer
+  );
+
+  await sendNotificationToUser(
+    "riders",
+    "A New Order Was Just Made!. Login to accept order",
+    sendToCustomer
+  );
+
+  // / update user noticfication
+  await saveDataToUserNotification(userId, docId, dataToSave)
+    .then(() => {
+      console.info("succesfully: saved notification data");
+    })
+    .catch((error) => {
+      console.info("error in execution: notification not saved");
+      console.log(error);
+      return { msg: "error in execution: notification not saved" };
+    });
+
+  // update like count
+  if (type === "food") {
+    // / add order to favourite
+    items.forEach(async (element) => {
+      const id = element.id;
+      const dataTo = {
+        likes_count: admin.firestore.FieldValue.increment(1),
+      };
+
+      await addDataToPopular(id, dataTo);
+    });
+  }
+
+  // update total count order count for that day
+  await incrementTotalOrderCount();
+
+  // update item fee
+  await incrementTotalOrderAmountCount(itemFee);
+
+  return Promise.resolve();
+};
+
+module.exports = onNewOrderCreated;
