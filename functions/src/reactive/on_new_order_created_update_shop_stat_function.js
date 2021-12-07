f55e65f4e4304ef04a146d0c2210d5e97e7ef207
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-12-07 09:54:10 +0100

    update

--- functions/src/reactive/on_new_order_created_update_shop_stat_function.js
+++ functions/src/reactive/on_new_order_created_update_shop_stat_function.js
@@ -1,5 +1,7 @@
 const admin = require("../../firebase_");
 const sendNotificationToUser = require("../controllers/send_notification_to_user");
+const updateShopWallet = require("../controllers/update_shop_wallet");
+const updateShopTotalNumberOfSales = require("../controllers/update_shop_total_number_of_sales");
 
 const OnNewOrderCreatedUpdateShopStat = async (snapshot, context) => {
   if (snapshot.after.data() === snapshot.before.data()) {
@@ -15,67 +17,74 @@ const OnNewOrderCreatedUpdateShopStat = async (snapshot, context) => {
   const itemFee = data.items_fee;
   const type = data.type;
 
-  const sendData = {
-    data_to_send: "msg_from_the_cloud",
-    click_action: "FLUTTER_NOTIFICATION_CLICK",
-    order_id: `${orderId}`,
-  };
+  try {
+    const sendData = {
+      data_to_send: "msg_from_the_cloud",
+      click_action: "FLUTTER_NOTIFICATION_CLICK",
+      order_id: `${orderId}`,
+    };
 
-  if (data.order_status === "accepted") {
-    items.forEach(async (element) => {
-      const FastFoodId = element.fast_food_id;
+    if (data.order_status === "accepted") {
+      items.forEach(async (element) => {
+        const FastFoodId = element.fast_food_id;
 
-      await sendNotificationToUser(
-        FastFoodId,
-        `New Order of ${element.name} has just been made! - vendor`,
-        sendData
-      );
-    });
-  }
+        await sendNotificationToUser(
+          FastFoodId,
+          `New Order of ${element.name} has just been made! - vendor`,
+          sendData
+        );
+      });
+    }
 
-  if (data.order_status !== "enroute") {
+    if (data.order_status !== "enroute") {
+      return Promise.resolve();
+    }
+
+    if (type === "food") {
+      // trying to update shop owner stat
+      // / add order to favourite
+      items.forEach(async (element) => {
+        const FastFoodId = element.fast_food_id;
+        const foodAmount = element.price;
+
+        const dataTo = {
+          wallet_balance: admin.firestore.FieldValue.increment(foodAmount),
+        };
+
+        await updateShopWallet(FastFoodId, dataTo);
+        await updateShopTotalNumberOfSales(FastFoodId, foodAmount);
+        // await sendNotificationToUser(
+        //   FastFoodId,
+        //   `Now Order of ${element.fast_food_name} has just been made!`,
+        //   sendData
+        // );
+      });
+    } else if (type === "market") {
+      items.forEach(async (element) => {
+        const FastFoodId = element.fast_food_id;
+        const foodAmount = element.price;
+
+        const dataTo = {
+          wallet_balance: admin.firestore.FieldValue.increment(foodAmount),
+        };
+
+        await updateShopWallet(FastFoodId, dataTo);
+        await updateShopTotalNumberOfSales(FastFoodId, foodAmount);
+        // await sendNotificationToUser(
+        //   FastFoodId,
+        //   `Now Order of ${element.fast_food_name} has just been made!`,
+        //   sendData
+        // );
+      });
+    }
+
     return Promise.resolve();
+  } catch (error) {
+    funtions.logger.error(
+      "error in execution: OnNewOrderCreatedUpdateShopStat"
+    );
+    funtions.logger.error(error);
   }
-
-  if (type === "food") {
-    // trying to update shop owner stat
-    // / add order to favourite
-    items.forEach(async (element) => {
-      const FastFoodId = element.fast_food_id;
-      const foodAmount = element.price;
-
-      const dataTo = {
-        wallet_balance: admin.firestore.FieldValue.increment(foodAmount),
-      };
-
-      await updateShopWallet(FastFoodId, dataTo);
-      await updateShopTotalNumberOfSales(FastFoodId, foodAmount);
-      // await sendNotificationToUser(
-      //   FastFoodId,
-      //   `Now Order of ${element.fast_food_name} has just been made!`,
-      //   sendData
-      // );
-    });
-  } else if (type === "market") {
-    items.forEach(async (element) => {
-      const FastFoodId = element.fast_food_id;
-      const foodAmount = element.price;
-
-      const dataTo = {
-        wallet_balance: admin.firestore.FieldValue.increment(foodAmount),
-      };
-
-      await updateShopWallet(FastFoodId, dataTo);
-      await updateShopTotalNumberOfSales(FastFoodId, foodAmount);
-      // await sendNotificationToUser(
-      //   FastFoodId,
-      //   `Now Order of ${element.fast_food_name} has just been made!`,
-      //   sendData
-      // );
-    });
-  }
-
-  return Promise.resolve();
 };
 
 module.exports = OnNewOrderCreatedUpdateShopStat;
